b422527bcb1430b9fcd4b65a9fcba8599852c872
Asaf <asafbm95@gmail.com>
2022-03-29 20:43:28 +0300

    add basic

--- get_data.py
+++ get_data.py
@@ -12,21 +12,6 @@ def get_apple_stock_api():
 def get_apple_stock_data():
     ts = TimeSeries(key=get_apple_stock_api(), output_format='pandas')
     data, meta_data = ts.get_daily(symbol='AAPL', outputsize = 'full')
-    data.to_csv('data/apple_daily_data',index=False)
+    print(meta_data)
+    data.to_csv('data/apple_daily_data')
     print(data)
-
-    i = 1
-    # while i==1:
-    #    data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
-    #    data.to_excel("output.xlsx")
-    #    time.sleep(60)
-
-    # close_data = data['4. close']
-    # percentage_change = close_data.pct_change()
-    #
-    # print(percentage_change)
-    #
-    # last_change = percentage_change[-1]
-    #
-    # if abs(last_change) > 0.0004:
-    #     print("MSFT Alert:" + str(last_change))
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import get_data
 
+def data_analysis(df):
+    """
 
+    :param df:
+    :return:
+    """
+    df.plot( x='date', y='close')
+    plt.show()
 
 if __name__ == "__main__":
+
     print("Hello world!")
-    get_data.get_apple_stock_data()
+    #get_data.get_apple_stock_data() # get/ update apple stock data
+    df = pd.read_csv('data/apple_daily_data')
+    df = df.rename(columns={'1. open': 'open', '2. high': 'high', '3. low': 'low',
+                            '4. close': 'close','5. volume': 'volume'})
+    print(df)
+    data_analysis(df)
