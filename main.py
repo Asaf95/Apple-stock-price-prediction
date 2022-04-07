ec14b208a2d58efb860554ca42a079bdc0655628
AsafB <asafbm95@gmail.com>
2022-04-07 17:26:19 +0300

    LSTM model works Try 07.04.2022 05:26 pm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import data_preparation
+import ml_classification_models
 
 
 
--- masho.py
+++ masho.py
@@ -7,28 +7,9 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import layers
 from copy import deepcopy
 import ml_classification_models
-import masho
-
-
-def apply_stock_split1(df, number, date):
-    list_of_columns = ['high', 'low', 'close', 'open']
-    list_of_columns1 = ['Volume']
-    for item in list_of_columns:
-        df['temp'] = df[(df['date'] < date)][item].div(number)
-        df['temp2'] = df['temp'].fillna(df[item])
-        df[item] = df['temp2']
-
-    df['temp'] = df[(df['date'] < date)]['volume'].div(1 / number)
-    df['temp2'] = df['temp'].fillna(df['volume'])
-    df['volume'] = df['temp2']
-    return df
 
 
 
-"""
-
-LSTM START
-"""
 
 def str_to_datetime(s):
     split = s.split('-')
@@ -175,77 +156,3 @@ def LSTM_model(q_80,q_90,dates_train, X_train, y_train,dates_val,
                 'Testing Observations',
                 'Recursive Predictions'])
     #plt.show()
-
-
-def LSTM_prepare_variables(df):
-
-    df_close = df.copy()
-    df_close = df_close[['date', 'close']]
-
-    df_close.index = df_close.pop('date')
-    df_close = df_close.iloc[::-1]
-    windowed_df = df_to_windowed_df(df_close,
-                                    '2021-03-25',
-                                    '2022-03-23',
-                                    n=3)
-    dates, X, y = windowed_df_to_date_X_y(windowed_df)
-
-    q_80 = int(len(dates) * .8)
-    q_90 = int(len(dates) * .9)
-
-    dates_train, X_train, y_train = dates[:q_80], X[:q_80], y[:q_80]
-    dates_val, X_val, y_val = dates[q_80:q_90], X[q_80:q_90], y[q_80:q_90]
-    dates_test, X_test, y_test = dates[q_90:], X[q_90:], y[q_90:]
-    masho.LSTM_model(q_80, q_90, dates_train, X_train, y_train, dates_val, X_val, y_val, dates_test, X_test, y_test)
-
-"""
-
-LSTM END
-"""
-
-
-
-def classification_models(df):
-
-    y1 = df['close']
-    y2 = df['volume']
-    X = df.iloc[:,2:4]
-    X = X.dropna(axis='columns') # Drop the columns where all elements are missing
-    y = y1
-    result = ml_classification_models.main(X, y)
-    print('------')
-    y = y2
-    result2 =ml_classification_models.main(X, y)
-
-
-def basic_prepare(df):
-    """
-
-    :param df:
-    :return:
-    """
-    df = df.rename(columns={'1. open': 'open', '2. high': 'high', '3. low': 'low',
-                            '4. close': 'close','5. volume': 'volume'})
-    df_org= df.copy()
-
-    df['date'] = df.apply(lambda x: pd.to_datetime(x['date'], format='%Y-%m-%d'), axis=1)
-
-
-    """
-    handle the stock splits problem
-    """
-    thisdict = {
-        "2020-08-31": 4,
-        "2014-06-9": 7,
-        "2005-02-28": 2,
-        "2000-06-21": 2,
-        "1987-06-16": 2
-    }
-    for key, value in thisdict.items():
-        df = apply_stock_split1(df,value, key)
-
-    df =df.drop(columns=['temp', 'temp2'])
-
-    LSTM_prepare_variables(df)
-    print(df)
-    classification_models(df)
