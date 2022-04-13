9e63e82d3d8f83506fee75d114544a266247b728
Asaf <asafbm95@gmail.com>
2022-04-13 22:00:53 +0300

    add some style

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Apple-stock-price-prediction
-Project for Data Mining Course
+We will predict the stock value of Apple 
+
+the main module calls all the other modules and have access to all the functions that are needed 
+for building the stock model
+the main three modules are:
+1. get_data()- using this module for getting the last stock price and the history prices.
+
+
+2. data_preparation()- using this module to prepare the data for the ML models by fixing the types of columns 
+fixing the stock splits issue and more.
+
+
+3. model_lstm()- predict the stock value and is the main ML model in this project.
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import pandas as pd
 import data_preparation
 import ml_classification_models
-import masho
+import model_lstm
+import get_data
 
 
+"""
+the main module calls all the other modules and have access to all the functions that are needed 
+for building the stock model
+the main three modules are:
+1. get_data()- using this module for getting the last stock price and the history prices.
+2. data_preparation()- using this module to prepare the data for the ML models by fixing the types of columns 
+fixing the stock splits issue and more.
+3. model_lstm()- predict the stock value and is the main ML model in this project.
+"""
 
-if __name__ == "__main__":
 
-    #get_data.get_apple_stock_data() # get/ update apple stock data
+def main():
+    get_data.get_apple_stock_data()
     raw_data = pd.read_csv('data/apple_daily_data')
     prepare_data = data_preparation.basic_prepare(raw_data)
-    recursive_predictions = masho.LSTM_prepare_variables(prepare_data)
+    recursive_predictions = model_lstm.LSTM_prepare_variables(prepare_data)
     print(recursive_predictions)
     results = ml_classification_models.classification_models(prepare_data)
     print(results)
+
+
+if __name__ == "__main__":
+    main()
